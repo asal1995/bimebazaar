f69f4831ec0d841d2263f8007a073b49ff1b726c
fx-asal <asalhnzd.1995@gmail.com>
2024-08-03 15:49:14 +0330

    Add implementation user data models and migrations file

--- migrations/env.py
+++ migrations/env.py
@@ -4,6 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from users.core.config import config as  env
+from users.db.database import Base
+import users.db.models as models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -15,24 +18,22 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
+
 target_metadata = Base.metadata
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
+print("Metadata tables:", target_metadata.tables)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-database_url = settings.async_db_url
+database_url = env.db_url
 
 config.set_main_option('sqlalchemy.url', database_url)
-
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
--- users/db/models.py
+++ users/db/models.py
@@ -1,17 +1,18 @@
-# db.py
-from sqlalchemy import Column, Integer, String, Boolean
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, Enum
+
 from users.db.database import Base
+from users.db.enums import USerTypeEnum
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean, server_default='true')
+    type_ = Column(Enum(USerTypeEnum), nullable=False, server_default=USerTypeEnum.USER.name)
 
     def __repr__(self):
         return f"<User email={self.email} active={self.is_active}>"
+
