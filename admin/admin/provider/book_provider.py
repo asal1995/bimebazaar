73dafc8e5f67df85c9fa85c016f5037e61cb1f64
fx-asal <asalhnzd.1995@gmail.com>
2024-08-06 11:18:07 +0330

    change the package of async http request

--- admin/admin/provider/book_provider.py
+++ admin/admin/provider/book_provider.py
@@ -1,4 +1,5 @@
-import httpx
+import aiohttp
+
 from admin.schemas.book_schema import BookCreate
 from admin.core.config import config
 
@@ -9,23 +10,24 @@ class BookProvider:
         self.base_url = config.book_url
 
     async def create_book(self, book: BookCreate):
-        async with httpx.AsyncClient() as client:
-            response = await client.post(
-                url=f"{self.base_url.strip('/')}/", json=book.model_dump(), headers=self.headers)
-            response.raise_for_status()
-            return response.json()
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.post(
+                    url=f"{self.base_url.strip('/')}/", json=book.model_dump(), headers=self.headers) as response:
+                response.raise_for_status()
+                return await response.json()
 
     async def update_book(self, book: BookCreate, book_id: int):
-        async with httpx.AsyncClient() as client:
-            response = await client.put(
-                url=f"{self.base_url.strip('/')}/{book_id}/", json=book.model_dump(), headers=self.headers)
-            response.raise_for_status()
-            return response.json()
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.put(
+                    url=f"{self.base_url.strip('/')}/{book_id}/", json=book.model_dump(),
+                    headers=self.headers) as response:
+                response.raise_for_status()
+                return await response.json()
 
     async def delete_book(self, book_id: int):
-        async with httpx.AsyncClient() as client:
-            response = await client.delete(
+        async with aiohttp.ClientSession(headers=self.headers) as session:
+            async with session.delete(
                 url=f"{self.base_url.strip('/')}/{book_id}/", headers=self.headers
-            )
-            response.raise_for_status()
-            return response.json()
+            ) as response:
+                response.raise_for_status()
+                return response.json()
--- admin/admin/routers/book_pro_endpoint.py
+++ admin/admin/routers/book_pro_endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import  HTTPException, Depends
+from fastapi import HTTPException, Depends
 import httpx
 
 from admin.core.security import get_current_user, User, role_required
@@ -7,7 +7,6 @@ from admin.schemas.book_schema import BookCreate
 from admin.provider.book_provider import BookProvider
 from admin.core.errors import CustomException
 from fastapi import APIRouter
-
 
 route = APIRouter()
 
@@ -27,8 +26,6 @@ async def create_book(
         return await provider.create_book(book)
     except CustomException as e:
         e.http_response()
-    except httpx.HTTPStatusError as e:
-        raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
 
 
 @route.put("/books/{book_id}/")
@@ -38,13 +35,10 @@ async def update_book(
         provider: BookProvider = Depends(get_book_provider),
         admin: User = Depends(get_current_user)
 ):
-
     try:
         return await provider.update_book(book, book_id)
     except CustomException as e:
         e.http_response()
-    except httpx.HTTPStatusError as e:
-        raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
 
 
 @route.delete("/books/{book_id}/")
@@ -57,5 +51,3 @@ async def delete_book(
         return await provider.delete_book(book_id)
     except CustomException as e:
         e.http_response()
-    except httpx.HTTPStatusError as e:
-        raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
